d88d72fa587babbed6a2e60c2b886de799bcf238
jemo <360835746@qq.com>
2018-07-27 10:40:17 +0800

    更改非弹出页面无法关闭

--- myscript.js
+++ myscript.js
@@ -1,9 +1,11 @@
-// var userAgent = navigator.userAgent;
-// if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
-//     window.location.href = "about:blank";
-// } else {
-//     window.opener = null;
-//     window.open("", "_self");
-//     window.close();
-// };
-window.close();
+var userAgent = navigator.userAgent;
+if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
+    window.location.href = "about:blank";
+} else {
+    window.opener = null;
+    window.open("", "_self");
+    window.close();
+};
+if (window.location.href == "about:blank") {
+    window.close();
+}
